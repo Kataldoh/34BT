ae0485e9c4391e31b1cf397e71fde5dc54ab1b25
Kataldoh <69595625+Kataldoh@users.noreply.github.com>
2022-03-15 15:51:00 +0100

    Update

---
+++ PROGETTO UNITY 34BT/Assets/Scripts/Sound/AudioType.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace UnityCore
+{
+    namespace Audio
+    {
+        public enum AudioType //In questo enum si possono definire tutti i tipi di audio 
+
+        //Ogni tipo di audio può succesivamente essere passato allo script del SoundManager
+
+        //
+        {
+            AMB_01,
+            ST_01,
+            ST_02,
+            SFX_PASSI_01,
+            SFX_02,
+            None,
+
+
+
+        }
+
+    }
+}
+
